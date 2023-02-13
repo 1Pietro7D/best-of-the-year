e40400fc19aa0b5dc0d7b07c24519bff8df5a569
PietroDanieli <1pie7d@gmail.com>
2023-02-13 16:53:51 +0100

    implement bootstrap, some fragment and generation of URL dinamic

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.Movie;
 import com.example.demo.Song;
 
 @Controller
-@RequestMapping("/home") // localhost:8080/home grestisce le CRUD
+@RequestMapping ("/") // localhost:8080/home grestisce le CRUD -- a differenza di GetMapping, RequestMapping richiede "/"
 public class MyController {
 	String name = "Pietro";
-	
+	/*
 	@GetMapping()
 	@ResponseBody // bad practice
 	public String index() {
@@ -24,8 +24,8 @@ public class MyController {
 				+ "<span>-    -<span>"
 				+ "<a href=\"http://localhost:8080/home/songs\"> Songs <a/>";
 	} 	
-	
-	@GetMapping("/index") // su questa rotta localhost:8080/home/index che va specificato INVECE su http://localhost:8080/ compare come statico
+	*/
+	@GetMapping // su questa rotta localhost:8080/home/index che va specificato INVECE su http://localhost:8080/ compare come statico
 	public String view(Model modelIndex) {
 		modelIndex.addAttribute("name", name);
 		return "index"; //questo pesca index.html su template
